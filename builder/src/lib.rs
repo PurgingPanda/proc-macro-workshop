bcd396566aa58ddc75102a8028d6532cc2417f60
PurgingPanda <t3ch0wn3r@gmail.com>
2023-09-04 20:41:11 +0200

    Update lib.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+use syn::{self, parse_macro_input};
+use syn::DeriveInput;
+use quote::{quote, ToTokens};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
+    let t = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    eprintln!("{:#?}", t);
+
+    quote!{}.into()
 }
